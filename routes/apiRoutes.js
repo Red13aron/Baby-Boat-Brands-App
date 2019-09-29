dff40b12668a88d176b9a6d48e18b055aeb1f143
Elianice Gorniak <eligorniak@gmail.com>
2019-09-29 18:45:56 -0500

    resolved conflicts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,25 +3,31 @@ var db = require("../models");
 module.exports = function(app) {
   // Get the API for specific user:
   app.get("/api/names/:userid", function(req, res) {
-    db.Names.findAll({ where: {
-      userId: req.params.userid }).then(function(dbnames) {
+    db.Names.findAll({
+      where: {
+        userId: req.params.userid
+      }
+    }).then(function(dbnames) {
       res.json(dbnames);
     });
   });
-// Get the API for the search terms in form HTML: not done!
+  // Get the API for the search terms in form HTML: not done!
   app.get("/api/names", function(req, res) {
-    db.Names.findAll({ where: {
-      userId: req.params.userid }).then(function(dbnames) {
+    db.Names.findAll({
+      where: {
+        userId: req.params.userid
+      }
+    }).then(function(dbnames) {
       res.json(dbnames);
     });
   });
 
-   // Get the API for all the names - just for developers:
+  // Get the API for all the names - just for developers:
   app.get("/api/developer/names", function(req, res) {
     db.Names.findAll({}).then(function(dbnames) {
       res.json(dbnames);
     });
-  }); 
+  });
 
   // Create a new example
   app.post("/api/names/:userid", function(req, res) {
