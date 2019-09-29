0295655eea5d7433f973943fb84d5f65ccafbb43
Elianice Gorniak <eligorniak@gmail.com>
2019-09-29 17:52:26 -0500

    update api routes, included package.json in .gitignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,38 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get the API for specific user:
+  app.get("/api/names/:userid", function(req, res) {
+    db.Names.findAll({ where: {
+      userId: req.params.userid }).then(function(dbnames) {
+      res.json(dbnames);
+    });
+  });
+// Get the API for the search terms in form HTML: not done!
+  app.get("/api/names", function(req, res) {
+    db.Names.findAll({ where: {
+      userId: req.params.userid }).then(function(dbnames) {
+      res.json(dbnames);
     });
   });
 
+   // Get the API for all the names - just for developers:
+  app.get("/api/developer/names", function(req, res) {
+    db.Names.findAll({}).then(function(dbnames) {
+      res.json(dbnames);
+    });
+  }); 
+
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/names/:userid", function(req, res) {
+    db.Names.create(req.body).then(function(dbnames) {
+      res.json(dbnames);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  app.delete("/api/names/:userid", function(req, res) {
+    db.Names.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.json(dbExample);
