767d2b21a43c2036bfe71eb41f92862dfeae33df
SNelson <snelson1337@gmail.com>
2019-09-29 16:47:24 -0500

    Added a seeds file and changed the schema

---
+++ models/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO names (userId, name, gender, searchTerm) VALUES (1, 'Meeses', 3, 'Cat');
+INSERT INTO names (userId, name, gender, searchTerm) VALUES (1, 'Bobbi', 2, 'Bright');
+INSERT INTO names (userId, name, gender, searchTerm) VALUES (1, 'Sylvester', 1, 'Of the Wood');
+INSERT INTO names (userId, name, gender, searchTerm) VALUES (1, 'Marilyn', null, 'Beloved');
+INSERT INTO names (name, gender, searchTerm) VALUES (1, 'John', 1, 'God is Gracious');
+
+
+INSERT INTO users (userName, userPassword) VALUES ('CeasarSmith', '1234Saladressing');
