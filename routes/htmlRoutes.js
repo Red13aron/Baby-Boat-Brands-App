b6a1657360af144008d455094c8c6e76d5375d83
SNelson <snelson1337@gmail.com>
2019-09-29 18:54:19 -0500

    Added routes for history and results

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/history", function(req, res) {
+    res.render("history");
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/results", function(req, res) {
+    res.render("results");
   });
 
   // Render 404 page for any unmatched routes
