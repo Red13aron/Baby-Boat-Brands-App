f7a95606d99a4b4a7917b61d2e2f7af2336e9f1b
Cameron Lattz <camerolattz@gmail.com>
2019-09-29 23:37:55 -0500

    fixing push

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,28 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    //db.Example.findAll({}).then(function(dbExamples) {
     res.render("index");
-    //});
   });
 
+  // Load example page and pass in an example by id
   app.get("/history", function(req, res) {
     res.render("history");
   });
 
-  app.get("/results/:term", function(req, res) {
-    res.render("results");
-  });
-
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/results", function(req, res) {
+    res.render("results");
   });
 
   // Render 404 page for any unmatched routes
