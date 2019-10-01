9d6b708991bf211f5e87e71791cb50fd1bae2ead
SNelson <snelson1337@gmail.com>
2019-10-01 10:13:13 -0500

    Got the results to post to the page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
-var db = require("../models");
+const Scrapper = require("../scrapper/scrappe.js");
+const scrapper = new Scrapper();
 
 module.exports = function(app) {
   // Load index page
@@ -11,11 +12,16 @@ module.exports = function(app) {
     res.render("history");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/results", function(req, res) {
-    res.render("results");
-  });
+  app.get("/results/:searchterm/:gender", async function(req, res) {
+    console.log("HI!");
+    const searchterm = req.params.searchterm;
+    const genderterm = req.params.gender;
 
+    const bNames = await scrapper.scrapper(searchterm, genderterm);
+    console.log(bNames);
+    const wrapper = { names: bNames };
+    res.render("results", wrapper);
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
