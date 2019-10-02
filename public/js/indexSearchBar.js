516d0c050aa0ff6cab34b0f75edb90dec1494813
Cameron Lattz <camerolattz@gmail.com>
2019-10-02 13:49:04 -0500

    fixed favoriting functionality, polishing styling

--- public/js/indexSearchBar.js
+++
@@ -1,7 +0,0 @@
-let input = document.getElementById("btns");
-input.addEventListener("OnClick", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("myBtn").click();
-  }
-});
